67a95f66f7b06aa0aed796c285abb77ea5ed1f2f
Kailynne-SI <kailynne.oliveira@gsuite.iff.edu.br>
2026-03-08 16:25:21 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
+const express = require("express");
+const app = express();
 
+app.get("/notificacao", (req, res) => {
+    res.json({
+        mensagem: "Nova notificação recebida!"
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000");
+});
